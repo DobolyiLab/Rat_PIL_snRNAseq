5e624e0f238c7e4597635f7f219579a2b5f33737
DobolyiLab <dora.fanni@semmelweis.hu>
2026-08-31 23:59:02 +0200

    Update files

--- config/paths.example.R
+++ config/paths.example.R
@@ -5,14 +5,14 @@
 # contain a hardcoded path -- if you find one, it is a bug, please report it.
 # =============================================================================
 
-r1_path <- "<path-to-data>/R1_Cont_with_WPRE/outs/filtered_feature_bc_matrix"
-r2_path <- "<path-to-data>/R2_Affi_with_WPRE/outs/filtered_feature_bc_matrix"
-r3_path <- "<path-to-data>/R3_Aggr_with_WPRE/outs/filtered_feature_bc_matrix"
+r1_path <- "<path-to-data>/R1_Cont/outs/filtered_feature_bc_matrix"
+r2_path <- "<path-to-data>/R2_Affi/outs/filtered_feature_bc_matrix"
+r3_path <- "<path-to-data>/R3_Aggr/outs/filtered_feature_bc_matrix"
 
 sample.dirs <- list(
-  "<path-to-data>/R1_Cont_with_WPRE",
-  "<path-to-data>/R2_Affi_with_WPRE",
-  "<path-to-data>/R3_Aggr_with_WPRE"
+  "<path-to-data>/R1_Cont",
+  "<path-to-data>/R2_Affi",
+  "<path-to-data>/R3_Aggr"
 )
 
 out_dir <- "<path-to-output>/outputs"
